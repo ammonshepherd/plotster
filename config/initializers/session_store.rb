3c6270788d8f433080dd4f801fe4487d82467872
Ammon Shepherd <ammon.shepherd@gmail.com>
2015-07-02 11:50:23 -0400

    Add file uploads with thumbnails and lat/long
    Get the browser's latitude and logitude using geoPosition.js from
    https://github.com/estebanav/javascript-mobile-desktop-geolocation and
    http://diveintohtml5.info/geolocation.html
    
    Allow file uploads with carrierwave
    
    Make thumbnails using mini_magick

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_newsus_session'
+Rails.application.config.session_store :cookie_store, key: '_plotster_session'
